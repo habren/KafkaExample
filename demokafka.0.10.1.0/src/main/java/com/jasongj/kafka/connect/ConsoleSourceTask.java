3109902ce15a17d5ace0b138547242e8db36db60
Jason Guo <jason.guo.vip@gmail.com>
2016-12-09 20:38:45 +0800

    Add ConsoleSourceTask

---
+++ demokafka.0.10.1.0/src/main/java/com/jasongj/kafka/connect/ConsoleSourceTask.java
@@ -0,0 +1,38 @@
+package com.jasongj.kafka.connect;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.connect.sink.SinkRecord;
+import org.apache.kafka.connect.sink.SinkTask;
+
+public class ConsoleSourceTask extends SinkTask {
+
+    @Override
+    public String version() {
+        return null;
+    }
+
+    @Override
+    public void flush(Map<TopicPartition, OffsetAndMetadata> arg0) {
+        
+    }
+
+    @Override
+    public void put(Collection<SinkRecord> arg0) {
+        
+    }
+
+    @Override
+    public void start(Map<String, String> arg0) {
+        
+    }
+
+    @Override
+    public void stop() {
+        
+    }
+
+}
