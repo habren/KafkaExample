473e613586aafc792786ae9f85ad8b97b3d324fc
Jason Guo <jason.guo.vip@gmail.com>
2016-12-14 22:23:36 +0800

    Implement ConsoleSourceTask

--- demokafka.0.10.1.0/src/main/java/com/jasongj/kafka/connect/ConsoleSourceTask.java
+++ demokafka.0.10.1.0/src/main/java/com/jasongj/kafka/connect/ConsoleSourceTask.java
@@ -1,38 +1,52 @@
 package com.jasongj.kafka.connect;
 
-import java.util.Collection;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
-import org.apache.kafka.clients.consumer.OffsetAndMetadata;
-import org.apache.kafka.common.TopicPartition;
-import org.apache.kafka.connect.sink.SinkRecord;
-import org.apache.kafka.connect.sink.SinkTask;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.source.SourceRecord;
+import org.apache.kafka.connect.source.SourceTask;
 
-public class ConsoleSourceTask extends SinkTask {
+public class ConsoleSourceTask extends SourceTask {
 
+    private InputStream inputStream;
+    private String topic;
+    
     @Override
     public String version() {
-        return new ConsoleSourceConnect().version();
+        return null;
     }
 
     @Override
-    public void flush(Map<TopicPartition, OffsetAndMetadata> arg0) {
-        
+    public List<SourceRecord> poll() throws InterruptedException {
+        try{
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            List<SourceRecord> records = new ArrayList<SourceRecord>();
+            while(reader.ready()){
+                String line = reader.readLine();
+//                SourceRecord record = new SourceRecord(null, null, topic, Schema.STRING_SCHEMA, line);
+            }
+            return records;
+        } catch(IOException ex) {
+            return null;
+        }
     }
 
     @Override
-    public void put(Collection<SinkRecord> arg0) {
-        
-    }
-
-    @Override
-    public void start(Map<String, String> arg0) {
-        
+    public void start(Map<String, String> conf) {
+        inputStream = System.in;
+        topic = conf.get("topic");
     }
 
     @Override
     public void stop() {
         
     }
+
 
 }
