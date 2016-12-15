52e81d269b0dfba2e62a909a5806a4ed7815b58a
Jason Guo <jason.guo.vip@gmail.com>
2016-12-15 19:05:52 +0800

    Update ConsoleSourceTask

--- demokafka.0.10.1.0/src/main/java/com/jasongj/kafka/connect/ConsoleSourceTask.java
+++ demokafka.0.10.1.0/src/main/java/com/jasongj/kafka/connect/ConsoleSourceTask.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -29,7 +30,7 @@ public class ConsoleSourceTask extends SourceTask {
             List<SourceRecord> records = new ArrayList<SourceRecord>();
             while(reader.ready()){
                 String line = reader.readLine();
-//                SourceRecord record = new SourceRecord(null, null, topic, Schema.STRING_SCHEMA, line);
+                SourceRecord record = new SourceRecord(Collections.singletonMap("SYSTEM_IN", null), Collections.singletonMap("OFFSET", 0), topic, Schema.STRING_SCHEMA, line);
             }
             return records;
         } catch(IOException ex) {
