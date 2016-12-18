24fe6423311af0bf686888c90516444c11622974
Jason Guo <jason.guo.vip@gmail.com>
2016-12-18 20:58:12 +0800

    Implement ConsoleSinkTask

--- demokafka.0.10.1.0/src/main/java/com/jasongj/kafka/connect/ConsoleSinkTask.java
+++ demokafka.0.10.1.0/src/main/java/com/jasongj/kafka/connect/ConsoleSinkTask.java
@@ -1,37 +1,46 @@
 package com.jasongj.kafka.connect;
 
+import java.io.PrintStream;
 import java.util.Collection;
 import java.util.Map;
 
 import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.utils.AppInfoParser;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.apache.kafka.connect.sink.SinkTask;
 
 public class ConsoleSinkTask extends SinkTask {
 
+    private PrintStream printStream;
+
     @Override
     public String version() {
-        return null;
+        return AppInfoParser.getVersion();
     }
 
     @Override
-    public void flush(Map<TopicPartition, OffsetAndMetadata> arg0) {
-
+    public void flush(Map<TopicPartition, OffsetAndMetadata> offsets) {
+        if(printStream != null){
+            printStream.flush();
+        }
     }
 
     @Override
-    public void put(Collection<SinkRecord> arg0) {
-
+    public void put(Collection<SinkRecord> records) {
+        records.forEach(printStream::println);
     }
 
     @Override
-    public void start(Map<String, String> arg0) {
-
+    public void start(Map<String, String> config) {
+        this.printStream = System.out;
     }
 
     @Override
     public void stop() {
+        if(printStream != null){
+            printStream.close();
+        }
 
     }
 
